f55553c17e7021dc70df30b2bab796ab0ed9310c
Paradigm Extra <135075234+paradigmextra@users.noreply.github.com>
2023-11-16 13:25:33 +0200

    Update 4-cmd_check.c

--- 4-cmd_check.c
+++ 4-cmd_check.c
@@ -1,62 +1,63 @@
 #include "main.h"
 
+#define MAX_ALIASES 10
+
+typedef struct {
+    char *name;
+    char *value;
+} Alias;
+
+Alias aliases[MAX_ALIASES];
+
 int check_cmd(char **input);
 
 /**
- * check_cmd - check if the command exist in the
- * path, if it does not creat a path and append it to the directory
- * @input: pointer to the command.
- *
- * Return: return a status value of 1 on success and 0 on failure.
+ * check_cmd - Check if the command exists in the path
+ * @input: Pointer to the command.
+ * Return: Return a status value of 1 on success and 0 on failure.
  */
-int check_cmd(char **input)
-{
-	char *path, *token, *bufa;
-	static char path_bufa[1024];
-	int i;
-	int num_aliases = 0;
+int check_cmd(char **input) {
+    char *path, *token, *bufa;
+    static char path_bufa[1024];
+    int i;
 
+    for (i = 0; i < MAX_ALIASES; i++) {
+        if (aliases[i].name != NULL && gb_strcmp(*input, aliases[i].name) == 0) {
+            *input = aliases[i].value; /**Replace the alias with its value**/
+            return 1;
+        }
+    }
 
-	for (i = 0; i < num_aliases; i++)
-	{
-		if (gb_strcmp(input[0], aliases[i].name) == 0)
-		{
-			input[0] = aliases[i].value; /**Replace the alias with its value**/
-			return (1);
-		}
-	}
+    path = getenv("PATH");
+    if (path == NULL || input == NULL || *input == NULL) {
+        return 0;
+    }
 
-	path = getenv("PATH");
-	if (path == NULL || *input == NULL || input == NULL)
-		return (0);
+    if (access(*input, X_OK) == 0 && (*input)[0] == '/') {
+        return 1;
+    }
 
-	if (access(*input, X_OK) == 0 && (*input)[0] == '/')
-	{
-		return (1);
-	}
+    gb_strcpy(path_bufa, path);
+    token = gb_strtok(path_bufa, ":");
+    while (token != NULL) {
+        bufa = malloc(gb_strlen(token) + gb_strlen(*input) + 2);
+        if (bufa == NULL) {
+            perror("malloc");
+            return 0;
+        }
 
+        gb_strcpy(bufa, token);
+        gb_strcat(bufa, "/");
+        gb_strcat(bufa, *input);
 
-	gb_strcpy(path_bufa, path);
-	token = gb_strtok(path_bufa, ":");
-	while (token != NULL)
-	{
-		bufa = malloc(gb_strlen(token) + gb_strlen(*input) + 2);
-		if (bufa == NULL)
-		{
-			perror("gb_strtok");
-			return (0);
-		}
+        if (access(bufa, X_OK) == 0) {
+            free(bufa);
+            return 1;
+        }
 
-		gb_strcpy(bufa, token);
-		gb_strcat(bufa, "/");
-		gb_strcat(bufa, *input);
+        free(bufa);
+        token = gb_strtok(NULL, ":");
+    }
 
-		if (access(bufa, X_OK) == 0)
-		{
-			return (1);
-		}
-		free(bufa);
-		token = gb_strtok(NULL, ":");
-	}
-	return (0);
+    return 0;
 }
