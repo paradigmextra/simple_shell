dce2f9e175312a4c353ff830d76c83a62b6d3bd6
Paradigm Extra <135075234+paradigmextra@users.noreply.github.com>
2023-11-17 00:57:36 +0200

    Update file_argument_handlers.c

--- file_argument_handlers.c
+++ file_argument_handlers.c
@@ -1,58 +1,79 @@
 #include "header.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-/**
- * read_file - Reads commands from the argument File
- * @file_name: File containing commands
- * @argv: Arguments passed
- * Return: -1 or 0
- */
-int read_file(const char *file_name, char **argv) {
-  FILE *fp = fopen(file_name, "r");
-  if (!fp) {
-    error_file(argv, 0);
-    return 127;
-  }
+void read_file(char *file, char **argv);
+void treat_file(char *line, int count, FILE *fp, char **argv);
+void exit_bul_for_file(char **cmd, char *line, FILE *fd);
 
-  char line[1024];
-  while (fgets(line, sizeof(line), fp)) {
-    treat_line(line, fp, argv);
-  }
+void read_file(char *file, char **argv)
+{
+    FILE *fp;
+    char *line = NULL;
+    size_t len = 0;
+    int count = 0;
 
-  fclose(fp);
-  return 0;
-}
-
-/**
- * treat_line - Parse and execute commands from a file line
- * @line: Line from file
- * @fp: File descriptor
- * @argv: Command line arguments
- */
-void treat_line(char *line, FILE *fp, char **argv) {
-  char *cmd[MAX_ARGS + 1];
-  int num_tokens = parse_cmd(line, cmd);
-
-  if (!strcmp(cmd[0], "exit")) {
-    exit_bul_for_file(cmd, fp);
-  } else if (check_builtin(cmd)) {
-    handle_builtin(cmd);
-  } else {
-    check_cmd(cmd, line, num_tokens, argv);
-  }
-}
-
-/**
- * exit_bul_for_file - Exit status handler for file input
- * @cmd: Parsed command
- * @fp: File Descriptor
- */
-void exit_bul_for_file(char **cmd, FILE *fp) {
-  if (!cmd[1]) {
+    fp = fopen(file, "r");
+    if (fp == NULL)
+    {
+        perror("Error opening file");
+        exit(1);
+    }
+    while ((getline(&line, &len, fp)) != -1)
+    {
+        count++;
+        treat_file(line, count, fp, argv);
+    }
+    if (line)
+        free(line);
     fclose(fp);
-    exit(errno);
-  }
+    exit(0);
+}
 
-  int status = atoi(cmd[1]);
-  fclose(fp);
-  exit(status);
+void treat_file(char *line, int count, FILE *fp, char **argv)
+{
+    char **cmd;
+    int stat = 0;
+
+    cmd = parse_cmd(line); // Assuming the `parse_cmd` function is defined elsewhere
+    if (strncmp(cmd[0], "exit", 4) == 0)
+        exit_bul_for_file(cmd, line, fp);
+    else if (check_builtin(cmd) == 0) // Assuming the `check_builtin` function is defined elsewhere
+    {
+        stat = handle_builtin(cmd, stat); // Assuming the `handle_builtin` function is defined elsewhere
+        free(cmd);
+    }
+    else
+    {
+        stat = check_cmd(cmd, line, count, argv); // Assuming the `check_cmd` function is defined elsewhere
+        free(cmd);
+    }
+}
+
+void exit_bul_for_file(char **cmd, char *line, FILE *fd)
+{
+    int status;
+    int i = 0;
+
+    if (cmd[i] == NULL)
+    {
+        free(line);
+        free(cmd);
+        fclose(fd);
+        exit(errno);
+    }
+    while (cmd[1][i])
+    {
+        if (isalpha(cmd[1][i++]) == 0) // Assuming the `_isalpha` function is defined elsewhere
+        {
+            perror("Illegal number");
+            exit(1);
+        }
+    }
+    status = atoi(cmd[1]); // Assuming the `_atoi` function is defined elsewhere
+    free(line);
+    free(cmd);
+    fclose(fd);
+    exit(status);
 }
