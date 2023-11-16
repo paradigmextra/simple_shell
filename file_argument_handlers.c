75caa38e3e1da3275ca887e60920ad5d23486117
Paradigm Extra <135075234+paradigmextra@users.noreply.github.com>
2023-11-17 00:42:07 +0200

    Update file_argument_handlers.c

--- file_argument_handlers.c
+++ file_argument_handlers.c
@@ -1,82 +1,91 @@
 #include "header.h"
 
 /**
- * read_file - Reads commands from the argument File
- * @file: File containing commands
- * @argv: Arguments passed
- * Return: -1 or 0
+ * @brief Reads commands from the specified file and executes them.
+ *
+ * @param file Filename containing commands.
+ * @param argv Command line arguments.
  */
 void read_file(char *file, char **argv)
 {
-	FILE *fp;
-	char *line = NULL;
-	size_t len = 0;
-	int count = 0;
+    FILE *fp = fopen(file, "r");
+    if (!fp)
+    {
+        error_file(argv, 0);
+        exit(127);
+    }
 
-	fp = fopen(file, "r");
-	if (fp == NULL)
-	{
-		error_file(argv, count);
-		exit(127);
-	}
-	while ((getline(&line, &len, fp)) != -1)
-	{
-		count++;
-		treat_file(line, count, fp, argv);
-	}
-	if (line)
-		free(line);
-	fclose(fp);
-	exit(0);
+    char *line = NULL;
+    size_t len = 0;
+    int count = 0;
+
+    while (getline(&line, &len, fp) != -1)
+    {
+        count++;
+        treat_file(line, count, fp, argv);
+    }
+
+    free(line);
+    fclose(fp);
+    exit(0);
 }
 
 /**
- * treat_file - Parse commands and handle their execution
- * @line: Line from file
- * @count: Error counter
- * @fp: File descriptor
- * @argv: Command line arguments
+ * @brief Parses commands from a file line and handles their execution.
+ *
+ * @param line Line from the file.
+ * @param count Error counter.
+ * @param fp File descriptor.
+ * @param argv Command line arguments.
  */
 void treat_file(char *line, int count, FILE *fp, char **argv)
 {
-	char **cmd;
-	int stat = 0;
+    char **cmd = parse_cmd(line);
+    int stat = 0;
 
-	cmd = parse_cmd(line);
-	if (_strncmp(cmd[0], "exit", 4) == 0)
-		exit_bul_for_file(cmd, line, fp);
-	else if (check_builtin(cmd) == 0)
-	{
-		stat = handle_builtin(cmd, stat);
-		free(cmd);
-	}
-	else
-	{
-		stat = check_cmd(cmd, line, count, argv);
-		free(cmd);
-	}
+    if (_strncmp(cmd[0], "exit", 4) == 0)
+        exit_bul_for_file(cmd, line, fp);
+    else if (check_builtin(cmd) == 0)
+    {
+        stat = handle_builtin(cmd, stat);
+        free(cmd);
+    }
+    else
+    {
+        stat = check_cmd(cmd, line, count, argv);
+        free(cmd);
+    }
 }
 
 /**
- * exit_bul_for_file - Exit status handler for file input
- * @line: Line from a file
- * @cmd: Parsed command
- * @fd: File Descriptor
+ * @brief Handles exit status for file input.
+ *
+ * @param cmd Parsed command.
+ * @param line Line from a file.
+ * @param fd File descriptor.
  */
 void exit_bul_for_file(char **cmd, char *line, FILE *fd)
 {
-	int status;
-	int i = 0;
+    int status = 0;
 
-	if (cmd[i] == NULL)
-	{
-		free(line);
-		free(cmd);
-		fclose(fd);
-		exit(errno);
-	}
-	while (cmd[1][i])
-	{
-		if (_isalpha(cmd[1][i++]) < 0)
-			perror("Illegal number");
+    if (cmd[1] == NULL)
+    {
+        free(line);
+        free(cmd);
+        fclose(fd);
+        exit(errno);
+    }
 
+    int i = 0;
+    while (cmd[1][i])
+    {
+        if (_isalpha(cmd[1][i++]) < 0)
+            perror("Illegal number");
+    }
+
+    status = _atoi(cmd[1]);
+    free(line);
+    free(cmd);
+    fclose(fd);
+    exit(status);
+}
